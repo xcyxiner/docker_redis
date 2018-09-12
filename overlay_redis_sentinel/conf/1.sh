bfb7d4e33968de04376c8681b9d61a7e01bf8a20
xcyxiner <593091948@qq.com>
2018-09-12 14:03:22 +0800

    overlay添加固定Ip

--- overlay_redis_sentinel/conf/1.sh
+++ overlay_redis_sentinel/conf/1.sh
@@ -2,4 +2,4 @@ cp sentinel.conf_bak  redis-sentinel.conf
 cp sentinel.conf_bak  redis-sentinel1.conf
 cp redis-slave.conf_bak  redis-slave1.conf
 cp redis-slave.conf_bak  redis-slave2.conf
-#sed -i 's/MASTERIP//g' redis-sentinel.conf redis-sentinel1.conf redis-slave1.conf redis-slave2.conf
+sed -i 's/MASTERIP/redis-master/g' redis-sentinel.conf redis-sentinel1.conf redis-slave1.conf redis-slave2.conf
